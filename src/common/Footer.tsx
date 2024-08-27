2ef5fe7c37d3023618d89eec416e096063b40d3b
QuocKhang <phamquockhang1108@gmail.com>
2024-08-27 13:06:08 +0700

    refactor: responsive for Footer

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -3,11 +3,12 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <div>
-      <footer className="flex justify-between items-center py-4 px-6 bg-white border-t border-gray-200">
-        <div className="text-sm text-gray-600">
+      <footer className="flex flex-col md:flex-row justify-between items-center py-4 px-6 bg-white border-t border-gray-200">
+        <div className="text-sm text-gray-600 m-5">
           <p>&copy; 2024 DaViKa Airways. All Rights Reserved.</p>
         </div>
-        <div className="text-sm text-gray-600">
+
+        <div className="text-sm text-gray-600 m-5">
           Payment methods
           <div className="flex items-center space-x-4">
             <img src="paypal_logo.png" alt="Paypal" className="h-8" />
@@ -15,7 +16,7 @@ const Footer: React.FC = () => {
           </div>
         </div>
 
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-4 m-5">
           <img src="dmca_logo.png" alt="DMCA Protected" className="h-8" />
           <img src="bocongthuong_logo.png" alt="Bộ Công Thương" className="h-12" />
         </div>
