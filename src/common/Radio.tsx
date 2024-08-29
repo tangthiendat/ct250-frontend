9ce36310e7593c8bb7cffe5aa9aa177335df72c9
QuocKhang <phamquockhang1108@gmail.com>
2024-08-30 03:48:55 +0700

    refactor: update Radio component to use useFormContext
    - Remove the 'options' prop from the Radio component
    - Update the component to use the useFormContext hook from react-hook-form
    - Add validation for the 'name' field using the register function
    - Update the rendering of the radio options with the new genderOptions array
    - Display an error message if the 'name' field is required and not filled in

--- src/common/Radio.tsx
+++ src/common/Radio.tsx
@@ -3,23 +3,45 @@ import { useFormContext } from "react-hook-form";
 
 interface RadioProps extends ComponentPropsWithoutRef<"input"> {
   name: string;
-  options: { value: string; label: string }[];
 }
 
-const Radio: React.FC<RadioProps> = ({ name, options, ...otherProps }) => {
-  const { register } = useFormContext();
-  return options.map((option) => (
-    <label key={option.value} className="flex-auto inline-flex items-center">
-      <input
-        type="radio"
-        value={option.value}
-        className="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
-        {...otherProps}
-        {...register(name)}
-      />
-      <span className="ml-2 text-sm">{option.label}</span>
-    </label>
-  ));
+const Radio: React.FC<RadioProps> = ({ name, ...otherProps }) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  const genderOptions = [
+    { value: "male", label: "Nam" },
+    { value: "female", label: "Nữ" },
+  ];
+
+  return (
+    <>
+      <label htmlFor={name} className="text label">
+        Giới tính
+      </label>
+      <div className="mt-3 flex justify-between gap-1 lg:justify-between lg:px-4">
+        {genderOptions.map((option) => (
+          <label key={option.value} className="inline-flex items-center">
+            <input
+              type="radio"
+              value={option.value}
+              className="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+              {...otherProps}
+              {...register(name, { required: "Giới tính không được để trống" })}
+            />
+            <span className="text ml-1">{option.label}</span>
+          </label>
+        ))}
+      </div>
+
+      {errors[name] && (
+        <p className="pt-1 text-sm text-red-500">
+          {String(errors[name].message)}
+        </p>
+      )}
+    </>
+  );
 };
 
 export default Radio;
