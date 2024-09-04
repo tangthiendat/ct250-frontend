6f8f035d160ec0fd619d315b0a5dd6dfd9a61a4d
QuocKhang <phamquockhang1108@gmail.com>
2024-09-04 21:46:59 +0700

    refactor: add ISearchFlights interface for booking module

---
+++ src/interfaces/booking/ISearchFlights.ts
@@ -0,0 +1,10 @@
+export interface ISearchFlights {
+  originAirport: string;
+  destinationAirport: string;
+  departureDate: string;
+  returnDate: string;
+  adultCount: number;
+  childrenCount: number;
+  infantCount: number;
+  cabinClass: string;
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,2 +1,3 @@
 export * from "./auth/IUser.ts";
 export * from "./auth/IAuthRequest.ts";
+export * from "./booking/ISearchFlights.ts";
