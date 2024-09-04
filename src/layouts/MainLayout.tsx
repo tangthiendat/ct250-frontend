787e8c36f7d3222d0a63c3bfddacb6c82bea3b76
QuocKhang <phamquockhang1108@gmail.com>
2024-09-04 21:45:53 +0700

    refactor: rename AuthLayout to MainLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom";
 import Footer from "../common/Footer";
 import Header from "../common/Header";
 
-const AuthLayout: React.FC = () => {
+const MainLayout: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Header />
       <div className="flex-grow">
         <Outlet />
@@ -14,4 +14,4 @@ const AuthLayout: React.FC = () => {
   );
 };
 
-export default AuthLayout;
+export default MainLayout;
