cd74a9f0c8463f1550df017fb595593971c72c39
tangthiendat <thiendattang@gmail.com>
2024-09-04 19:36:58 +0700

    feat: set up react query and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRouter from "./router/AppRouter";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 function App() {
-  return <AppRouter />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <AppRouter />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
