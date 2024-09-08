14adf746445bdfa82ba659b2680bb1115d824070
QuocKhang <phamquockhang1108@gmail.com>
2024-09-08 21:26:55 +0700

    refactor: update component to use antd

--- src/features/auth/LoginForm.tsx
+++ src/features/auth/LoginForm.tsx
@@ -1,71 +1,89 @@
-import { FormProvider, useForm } from "react-hook-form";
-import { IAuthRequest } from "../../interfaces";
-import Input from "../../common/Input";
+import { Form, Input } from "antd";
+import { SizeType } from "antd/es/config-provider/SizeContext";
+import { useState } from "react";
 
 const LoginForm: React.FC = () => {
-  const methods = useForm<IAuthRequest>({
-    mode: "onChange",
-  });
-  const { formState } = methods;
+  const [componentSize, setComponentSize] = useState<SizeType | "default">(
+    "default",
+  );
 
-  function onLogin(data: IAuthRequest): void {
+  const onFormLayoutChange = ({ size }: { size: SizeType }) => {
+    setComponentSize(size);
+  };
+
+  function onSubmit(data: any): void {
     console.log(data);
   }
 
   return (
-    <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onLogin)} className="space-y-5">
-        <div>
-          <Input
-            type="text"
-            name="email"
-            label="Email"
-            placeholder="Nhập email của bạn"
-            validation={{
-              required: "Email không được để trống",
-            }}
-          />
-        </div>
+    <Form
+      className="flex flex-col"
+      onFinish={onSubmit}
+      layout="vertical"
+      initialValues={{ size: componentSize }}
+      onValuesChange={onFormLayoutChange}
+      size={componentSize as SizeType}
+    >
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: "Vui lòng nhập email",
+          },
+          {
+            pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+            message: "Email không hợp lệ",
+          },
+        ]}
+      >
+        <Input placeholder="Email" />
+      </Form.Item>
 
-        <div>
-          <Input
-            name="password"
-            label="Mật khẩu"
-            placeholder="Nhập mật khẩu của bạn"
-            validation={{
-              required: "Mật khẩu không được để trống",
-            }}
-          />
-        </div>
+      <Form.Item
+        label="Mật khẩu"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Vui lòng nhập mật khẩu",
+          },
+        ]}
+      >
+        <Input.Password placeholder="Mật khẩu" />
+      </Form.Item>
 
-        <div>
-          <button
-            type="submit"
-            className={`focus:shadow-outline mt-2 w-full rounded py-2 font-bold text-white focus:outline-none ${!formState.isValid ? "bg-blue-300" : "bg-blue-700 hover:bg-blue-900"}`}
-            disabled={!formState.isValid}
-          >
-            Đăng nhập
-          </button>
-        </div>
-        <div className="text-center text-xs">
-          <a
-            href="#"
-            className="text-sm font-semibold text-blue-700 hover:text-blue-900"
-          >
-            Quên mật khẩu?
-          </a>
-        </div>
-        <div className="text-center text-xs">
-          <span className="text-sm text-gray-900">Chưa có tài khoản? </span>
+      <Form.Item>
+        <button
+          type="submit"
+          className="focus:shadow-outline mt-2 w-full rounded bg-blue-700 py-2 font-bold text-white hover:bg-blue-900 focus:outline-none"
+        >
+          Đăng nhập
+        </button>
+      </Form.Item>
+
+      {/* <div className="text-center text-xs"></div> */}
+
+      <div className="flex flex-col gap-5 text-center text-xs">
+        <a
+          href="#"
+          className="text-sm font-semibold text-blue-700 hover:text-blue-900"
+        >
+          Quên mật khẩu?
+        </a>
+
+        <span className="text-sm text-gray-900">
+          Chưa có tài khoản? {""}
           <a
             href="../register"
             className="text-sm font-semibold text-blue-700 hover:text-blue-900"
           >
             Đăng ký ngay
           </a>
-        </div>
-      </form>
-    </FormProvider>
+        </span>
+      </div>
+    </Form>
   );
 };
 
--- src/features/auth/RegisterForm.tsx
+++ src/features/auth/RegisterForm.tsx
@@ -1,8 +1,6 @@
-import { FormProvider, useForm } from "react-hook-form";
 import { IUser } from "../../interfaces";
-import Input from "../../common/Input";
-import Radio from "../../common/Radio";
-import Select from "../../common/Select";
+import { Form, Input, Radio, Select, DatePicker } from "antd";
+import { useState } from "react";
 
 const countryOptions = [
   { value: "vietnam", label: "Việt Nam" },
@@ -17,200 +15,242 @@ const genderOptions = [
   { value: "female", label: "Nữ" },
 ];
 
+type SizeType = Parameters<typeof Form>[0]["size"];
+
 const RegisterForm: React.FC = () => {
-  const methods = useForm<IUser>({
-    mode: "onChange",
-  });
-  // const { formState } = methods;
+  const [componentSize, setComponentSize] = useState<SizeType | "default">(
+    "default",
+  );
+
+  const onFormLayoutChange = ({ size }: { size: SizeType }) => {
+    setComponentSize(size);
+  };
 
   function onSubmit(data: IUser): void {
     console.log(data);
   }
 
   return (
-    <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-6">
-        <div className="max-lg:space-y-6 lg:flex lg:gap-10">
-          <div className="flex-1">
-            <Input
-              type="text"
-              name="lastName"
-              label="Họ"
-              placeholder="Họ, ví dụ PHAM"
-              style={{ textTransform: "uppercase" }}
-              validation={{
-                required: "Vui lòng nhập họ",
-                pattern: {
-                  value:
-                    /^[a-záàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệóòỏõọôốồổỗộơớờởỡợíìỉĩịúùủũụưứừửữựýỳỷỹỵđA-ZÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬÉÈẺẼẸÊẾỀỂỄỆÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢÍÌỈĨỊÚÙỦŨỤƯỨỪỬỮỰÝỲỶỸỴĐ\s]+$/,
-                  message: "Họ không chứa ký tự đặc biệt",
-                },
-                maxLength: {
-                  value: 30,
-                  message: 'Chú ý nhập "Họ" đúng với thông tin trên CCCD',
-                },
-              }}
-            />
-          </div>
-          <div className="flex-1">
-            <Input
-              type="text"
-              name="firstName"
-              label="Tên đệm & tên"
-              placeholder="Tên đệm & tên, ví dụ VAN A"
-              style={{ textTransform: "uppercase" }}
-              validation={{
-                required: "Vui lòng nhập tên",
-                pattern: {
-                  value:
-                    /^[a-záàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệóòỏõọôốồổỗộơớờởỡợíìỉĩịúùủũụưứừửữựýỳỷỹỵđA-ZÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬÉÈẺẼẸÊẾỀỂỄỆÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢÍÌỈĨỊÚÙỦŨỤƯỨỪỬỮỰÝỲỶỸỴĐ\s]+$/,
-                  message: "Tên không chứa ký tự đặc biệt",
-                },
-                maxLength: {
-                  value: 30,
-                  message:
-                    'Chú ý nhập "Tên đệm & tên " đúng với thông tin trên CCCD',
-                },
-              }}
-            />
-          </div>
-        </div>
-
-        <div className="flex gap-10">
-          <div className="flex-1">
-            <Input
-              type="date"
-              name="dob"
-              label="Ngày sinh"
-              validation={{
-                required: "Vui lòng nhập ngày sinh",
-                max: {
-                  value: new Date().toISOString().split("T")[0],
-                  message: "Ngày sinh không hợp lệ",
-                },
-              }}
-            />
-          </div>
-
-          <div className="flex-1">
-            <Radio
-              label="Giới tính"
-              options={genderOptions}
-              name="gender"
-              validation={{
-                required: "Vui lòng chọn giới tính",
-              }}
-            />
-          </div>
-        </div>
-
-        <div className="max-lg:space-y-6 lg:flex lg:gap-10">
-          <div className="flex-1">
-            <Input
-              type="text"
-              name="phoneNumber"
-              label="Số điện thoại"
-              placeholder="Số điện thoại"
-              validation={{
-                required: "Vui lòng nhập số điện thoại",
-                pattern: {
-                  value: /(84|0[3|5|7|8|9])+([0-9]{8})\b/,
-                  message: "Số điện thoại không hợp lệ",
-                },
-              }}
-            />
-          </div>
-          <div className="flex-1">
-            <Select options={countryOptions} name="country" label="Quốc gia" />
-          </div>
-        </div>
-
-        <div>
+    <Form
+      className="flex flex-col"
+      onFinish={onSubmit}
+      layout="vertical"
+      initialValues={{ size: componentSize }}
+      onValuesChange={onFormLayoutChange}
+      size={componentSize as SizeType}
+    >
+      <div className="flex gap-5">
+        <Form.Item
+          className="flex-1"
+          label="Họ"
+          name="lastName"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng nhập họ",
+            },
+            {
+              pattern:
+                /^[a-záàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệóòỏõọôốồổỗộơớờởỡợíìỉĩịúùủũụưứừửữựýỳỷỹỵđA-ZÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬÉÈẺẼẸÊẾỀỂỄỆÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢÍÌỈĨỊÚÙỦŨỤƯỨỪỬỮỰÝỲỶỸỴĐ\s]+$/,
+              message: "Họ không chứa ký tự đặc biệt",
+            },
+          ]}
+        >
           <Input
-            type="text"
-            name="passportNumber"
-            label="Hộ chiếu/CCCD"
-            placeholder="Hộ chiếu/CCCD"
-            validation={{
-              required: "Vui lòng nhập số hộ chiếu/CCCD",
-              pattern: {
-                value: /^[0-9]{9,12}$/,
-                message: "Số hộ chiếu/CCCD không hợp lệ",
-              },
-            }}
+            placeholder="Họ, ví dụ PHAM"
+            style={{ textTransform: "uppercase" }}
           />
-        </div>
+        </Form.Item>
 
-        <div>
+        <Form.Item
+          className="flex-1"
+          label="Tên đệm & tên"
+          name="firstName"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng nhập tên đệm & tên",
+            },
+            {
+              pattern:
+                /^[a-záàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệóòỏõọôốồổỗộơớờởỡợíìỉĩịúùủũụưứừửữựýỳỷỹỵđA-ZÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬÉÈẺẼẸÊẾỀỂỄỆÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢÍÌỈĨỊÚÙỦŨỤƯỨỪỬỮỰÝỲỶỸỴĐ\s]+$/,
+              message: "Họ không chứa ký tự đặc biệt",
+            },
+          ]}
+        >
           <Input
-            type="email"
-            name="email"
-            label="Email"
-            placeholder="Email"
-            validation={{
-              required: "Vui lòng nhập email",
-              pattern: {
-                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                message: "Email không hợp lệ",
-              },
-            }}
+            placeholder="Tên đệm & tên, ví dụ VAN A"
+            style={{ textTransform: "uppercase" }}
           />
-        </div>
+        </Form.Item>
+      </div>
 
-        <div className="max-lg:space-y-6 lg:flex lg:gap-10">
-          <div className="flex-1">
-            <Input
-              name="password"
-              label="Mật khẩu"
-              placeholder="Mật khẩu"
-              validation={{
-                required: "Vui lòng nhập mật khẩu",
-                minLength: {
-                  value: 6,
-                  message: "Mật khẩu phải chứa ít nhất 6 ký tự",
-                },
-              }}
-            />
-          </div>
-          <div className="flex-1">
-            <Input
-              name="confirmPassword"
-              label="Xác nhận mật khẩu"
-              placeholder="Xác nhận mật khẩu"
-              validation={{
-                required: "Vui lòng xác nhận mật khẩu",
-                validate: (value: string) =>
-                  value === methods.getValues("password") ||
-                  "Mật khẩu không trùng khớp",
-              }}
-            />
-          </div>
-        </div>
+      <div className="flex gap-5">
+        <Form.Item
+          className="flex-1"
+          label="Ngày sinh"
+          name="dob"
+          rules={[
+            {
+              required: true,
+              message: "Ngày sinh không hợp lệ",
+              validator: (_, value) =>
+                new Date(value).getTime() < new Date().getTime()
+                  ? Promise.resolve()
+                  : Promise.reject("Ngày sinh không hợp lệ"),
+            },
+          ]}
+        >
+          <DatePicker className="w-full" />
+        </Form.Item>
 
-        <div>
-          <button
-            type="submit"
-            className="focus:shadow-outline mt-2 w-full rounded bg-blue-700 py-2 font-bold text-white hover:bg-blue-900 focus:outline-none"
-            // className={`focus:shadow-outline mt-2 w-full rounded py-2 font-bold text-white focus:outline-none ${!formState.isValid ? "bg-blue-300" : "bg-blue-700 hover:bg-blue-900"}`}
-            // disabled={!formState.isValid}
-          >
-            Đăng ký
-          </button>
-        </div>
+        <Form.Item
+          className="flex-1"
+          label="Giới tính"
+          name="gender"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng chọn giới tính",
+            },
+          ]}
+        >
+          <Radio.Group className="space-x-4" options={genderOptions} />
+        </Form.Item>
+      </div>
 
-        <div className="text-center text-xs">
-          <span className="text-sm text-gray-900">
-            Bạn đã có tài khoản? Quay lại{" "}
-          </span>
-          <a
-            href="../login"
-            className="text-sm font-semibold text-blue-700 hover:text-blue-900"
-          >
-            đăng nhập
-          </a>
-        </div>
-      </form>
-    </FormProvider>
+      <div className="flex gap-5">
+        <Form.Item
+          className="flex-1"
+          label="Số điện thoại"
+          name="phoneNumber"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng nhập số điện thoại",
+            },
+            {
+              pattern: /(84|0[3|5|7|8|9])+([0-9]{8})\b/,
+              message: "Số điện thoại không hợp lệ",
+            },
+          ]}
+        >
+          <Input placeholder="Số điện thoại" />
+        </Form.Item>
+
+        <Form.Item
+          className="flex-1"
+          label="Quốc gia"
+          name="country"
+          rules={[
+            {
+              required: true,
+              message: "Vui lòng chọn quốc gia",
+            },
+          ]}
+        >
+          <Select
+            placeholder="Vui lòng chọn quốc gia"
+            options={countryOptions}
+          />
+        </Form.Item>
+      </div>
+
+      <Form.Item
+        label="Hộ chiếu/CCCD"
+        name="passportNumber"
+        rules={[
+          {
+            required: true,
+            message: "Vui lòng nhập số hộ chiếu/CCCD",
+          },
+          {
+            pattern: /^[0-9]{9,12}$/,
+            message: "Số hộ chiếu/CCCD không hợp lệ",
+          },
+        ]}
+      >
+        <Input placeholder="Hộ chiếu/CCCD" />
+      </Form.Item>
+
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: "Vui lòng nhập email",
+          },
+          {
+            pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+            message: "Email không hợp lệ",
+          },
+        ]}
+      >
+        <Input placeholder="Email" />
+      </Form.Item>
+
+      <Form.Item
+        label="Mật khẩu"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Vui lòng nhập mật khẩu",
+          },
+          {
+            min: 6,
+            message: "Mật khẩu phải chứa ít nhất 6 ký tự",
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password placeholder="Mật khẩu" />
+      </Form.Item>
+
+      <Form.Item
+        label="Xác nhận mật khẩu"
+        name="confirmPassword"
+        dependencies={["password"]}
+        rules={[
+          {
+            required: true,
+            message: "Vui lòng xác nhận mật khẩu",
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue("password") === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error("Mật khẩu xác nhận không khớp!"));
+            },
+          }),
+        ]}
+        hasFeedback
+      >
+        <Input.Password placeholder="Xác nhận mật khẩu" />
+      </Form.Item>
+
+      <Form.Item>
+        <button
+          type="submit"
+          className="focus:shadow-outline mt-2 w-full rounded bg-blue-700 py-2 font-bold text-white hover:bg-blue-900 focus:outline-none"
+        >
+          Đăng ký
+        </button>
+      </Form.Item>
+      <div className="text-center text-xs">
+        <span className="text-sm text-gray-900">
+          Bạn đã có tài khoản? Quay lại{" "}
+        </span>
+        <a
+          href="../login"
+          className="text-sm font-semibold text-blue-700 hover:text-blue-900"
+        >
+          đăng nhập
+        </a>
+      </div>
+    </Form>
   );
 };
 
