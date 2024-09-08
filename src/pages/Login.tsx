013208ae652536ee9641e1c6516819e724a6186d
QuocKhang <phamquockhang1108@gmail.com>
2024-09-08 21:28:04 +0700

    refactor: update Login and Register pages UI

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,16 +2,26 @@ import LoginForm from "../features/auth/LoginForm";
 
 const Login: React.FC = () => {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="mx-auto rounded-xl border bg-slate-100 px-20 py-10 shadow-md">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mb-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Đăng nhập
-          </h2>
-        </div>
+    <div className="flex">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        {/* mx-auto w-1/2 min-w-96  */}
+        <div className="flex items-center justify-between rounded-xl bg-white p-8 lg:border lg:shadow-md">
+          <div className="mx-auto min-w-96 rounded-xl border bg-slate-100 p-5 shadow lg:w-1/2">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-8 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Đăng nhập
+              </h2>
+            </div>
 
-        <div className="mt-1 sm:mx-auto sm:w-full sm:max-w-sm">
-          <LoginForm />
+            {/* <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm"></div> */}
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-xl">
+              <LoginForm />
+            </div>
+          </div>
+
+          <div className="hidden w-1/2 lg:block">
+            <img src="/login_background.jpg" alt="" />
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,23 +1,31 @@
 import React from "react";
 import RegisterForm from "../features/auth/RegisterForm";
+
 const Register: React.FC = () => {
   return (
-    <>
+    <div className="flex">
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="bg-slate-100 p-8 border rounded-xl shadow-md w-1/2 mx-auto min-w-96">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-8 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Đăng ký
-            </h2>
+        {/* mx-auto w-1/2 min-w-96  */}
+        <div className="flex items-center justify-between rounded-xl bg-white p-8 lg:border lg:shadow-md">
+          <div className="hidden w-1/2 lg:block">
+            <img src="/register_background.jpg" alt="" className="" />
           </div>
 
-          {/* <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm"></div> */}
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-xl">
-            <RegisterForm />
+          <div className="mx-auto min-w-96 rounded-xl border bg-slate-100 p-5 shadow lg:w-1/2">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-8 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Đăng ký
+              </h2>
+            </div>
+
+            {/* <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm"></div> */}
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-xl">
+              <RegisterForm />
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
