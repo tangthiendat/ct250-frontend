0b74a83d945fdb2a77c1b9439a617ecd57573410
Nqvinh7603 <nqvinh7603@gmail.com>
2024-09-17 19:01:43 +0700

    fix: automatically return form data when logging in with googleregister form

--- src/features/auth/RegisterForm.tsx
+++ src/features/auth/RegisterForm.tsx
@@ -1,11 +1,49 @@
-import { Button, Divider, Form } from "antd";
-import React from "react";
-import GoogleLoginHandler from "./components/GoogleLoginHandler";
-import RegisterFormFields from "./components/RegisterFromFields";
+import { Button, Divider, Form, notification } from "antd";
+import React, { useState } from "react";
+
+import ContactFields from "./components/ContactFields";
+import DateAndGenderFields from "./components/DateAndGenderFields";
+import GoogleLoginButton from "./components/GoogleLoginButton";
+import NameFields from "./components/NameFields";
+import PasswordFields from "./components/PasswordFields";
 import { useRegisterForm } from "./hooks/UseRegisterForm";
+import IdentityFields from "./components/IndentityFields";
 
 const RegisterForm: React.FC = () => {
   const { form, isLoading, contextHolder, onFinish } = useRegisterForm();
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+
+  const handleGoogleLoginSuccess = async (tokenResponse: any) => {
+    const userInfoResponse = await fetch(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${tokenResponse.access_token}`,
+        },
+      },
+    );
+    const profile = await userInfoResponse.json();
+
+    localStorage.removeItem("avatarUrl");
+
+    form.setFieldsValue({
+      email: profile.email,
+      firstName: profile.family_name,
+      lastName: profile.given_name,
+      avatar: profile.picture,
+    });
+    localStorage.setItem("avatarUrl", profile.picture);
+    setAvatarUrl(profile.picture);
+    console.log("Google login profile:", profile);
+  };
+
+  const handleGoogleLoginFailure = (error: any) => {
+    console.error("Google login error:", error);
+    notification.error({
+      message: "Đăng nhập Google thất bại",
+      description: "Có lỗi xảy ra trong quá trình đăng nhập Google",
+    });
+  };
 
   return (
     <>
@@ -17,7 +55,14 @@ const RegisterForm: React.FC = () => {
         size="large"
         form={form}
       >
-        <RegisterFormFields />
+        <NameFields />
+        <DateAndGenderFields />
+        <ContactFields />
+        <IdentityFields />
+        <PasswordFields />
+        <Form.Item name="avatar" hidden>
+          <input type="hidden" />
+        </Form.Item>
 
         <Button
           type="primary"
@@ -31,7 +76,10 @@ const RegisterForm: React.FC = () => {
 
         <Divider plain>Hoặc</Divider>
 
-        <GoogleLoginHandler />
+        <GoogleLoginButton
+          onSuccess={handleGoogleLoginSuccess}
+          onError={handleGoogleLoginFailure}
+        />
       </Form>
     </>
   );
