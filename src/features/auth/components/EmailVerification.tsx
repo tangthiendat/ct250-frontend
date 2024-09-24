f1823501ad604bb4d2e29d16b7696f66d3ee1cd9
tangthiendat <thiendattang@gmail.com>
2024-09-24 08:44:07 +0700

    refactor: email verification

--- src/features/auth/components/EmailVerification.tsx
+++ src/features/auth/components/EmailVerification.tsx
@@ -3,45 +3,40 @@ import React, { useEffect, useRef, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import Loading from "../../../common/Loading";
 import { authService } from "../../../services/auth-service";
+import { useMutation } from "@tanstack/react-query";
 
 const EmailVerification: React.FC = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
   const hasVerified = useRef(false);
-  const [loading, setLoading] = useState(true);
+  const { mutate: verifyEmail, isPending: isVerifying } = useMutation({
+    mutationFn: authService.verifyEmail,
+    onSuccess: () => {
+      notification.success({
+        pauseOnHover: true,
+        message: "Xác thực email thành công",
+        description: "Email của bạn đã được xác thực. Vui lòng đăng nhập.",
+      });
+      navigate("/login");
+    },
+    onError: (error: any) => {
+      notification.error({
+        message: "Xác thực email thất bại",
+        description: "Có lỗi xảy ra trong quá trình xác thực email.",
+      });
+    },
+  });
 
   useEffect(() => {
-    const verifyEmail = async () => {
-      if (token && !hasVerified.current) {
-        hasVerified.current = true;
-        try {
-          await authService.verifyEmail(token);
-          setTimeout(() => {
-            notification.success({
-              pauseOnHover: true,
-              message: "Xác thực email thành công",
-              description:
-                "Email của bạn đã được xác thực. Vui lòng đăng nhập.",
-            });
-            setLoading(false);
-            navigate("/login");
-          }, 3000);
-        } catch (error: any) {
-          notification.error({
-            message: "Xác thực email thất bại",
-            description: "Có lỗi xảy ra trong quá trình xác thực email.",
-          });
-          setLoading(false);
-        }
-      }
-    };
-
-    verifyEmail();
-  }, [token, navigate]);
+    if (token && !hasVerified.current) {
+      hasVerified.current = true;
+      verifyEmail(token);
+    }
+  }, [token, verifyEmail]);
 
   return (
-    <>{loading && <Loading message="Tài khoản đang được kích hoạt..." />}</>
+    <>{isVerifying && <Loading message="Tài khoản đang được kích hoạt..." />}</>
   );
 };
 
