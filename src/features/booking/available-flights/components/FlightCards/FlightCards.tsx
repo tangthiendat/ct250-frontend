42c92384fce17f6efa3ca6cd2fe2403fe050b3b2
QuocKhang <phamquockhang1108@gmail.com>
2024-10-15 09:20:21 +0700

    refactor: handle unavailable seat flights

--- src/features/booking/available-flights/components/FlightCards/FlightCards.tsx
+++ src/features/booking/available-flights/components/FlightCards/FlightCards.tsx
@@ -16,11 +16,24 @@ const FlightCards: React.FC = () => {
     queryFn: () => flightScheduleService.search(criteria),
   });
 
-  const flights = flightsData?.payload || [];
+  // const flights = flightsData?.payload || [];
+  const tempFlights = flightsData?.payload || [];
+  const flights = tempFlights.filter(
+    (flight) =>
+      flight.seatAvailability.filter((seat) => seat.status === "AVAILABLE")
+        .length > 0,
+  );
+
   // console.log(flights);
 
   return (
     <div className="mx-auto mt-10 max-w-screen-md transition-all duration-1000 xl:max-w-screen-lg">
+      {flights.length === 0 && (
+        <div className="text-heading-2 text-gray-500">
+          😥Chuyến bay vào ngày bạn chọn đã hết chỗ. Vui lòng chọn ngày
+          khác!!!😊
+        </div>
+      )}
       {flights?.map((flight) => (
         <div className="mt-8">
           <FlightCard key={flight.flightId} flightCardData={flight} />
--- src/features/booking/available-flights/components/FlightCards/components/FlightCardClassOptions.tsx
+++ src/features/booking/available-flights/components/FlightCards/components/FlightCardClassOptions.tsx
@@ -1,4 +1,4 @@
-import { MdExpandMore } from "react-icons/md";
+import { MdExpandMore, MdNotInterested } from "react-icons/md";
 import { IFlightSchedule, TicketClass } from "../../../../../../interfaces";
 
 interface FlightCardClassOptionsProps {
@@ -31,67 +31,90 @@ const FlightCardClassOptions: React.FC<FlightCardClassOptionsProps> = ({
   ).length;
   return (
     <>
-      <div
-        className="relative flex-1 cursor-pointer bg-green-700"
-        onClick={() => {
-          if (showEconomyClass === false) {
-            handleShowEconomyClass();
-          } else {
-            handleCloseDetailClass();
-          }
-        }}
-      >
-        <div className="flex flex-col items-center py-2 text-white">
-          <div className="absolute -top-[24px] rounded-t-md bg-green-900 px-2 py-1">
-            <p className="text-xs">{`Còn ${availableEconomySeats} chỗ`}</p>
+      {availableEconomySeats == 0 ? (
+        <div className="relative flex flex-1 cursor-not-allowed items-center justify-center bg-gray-300">
+          <div className="flex flex-col items-center py-8 text-black opacity-70">
+            <p className="font-bold text-green-800">Economy</p>
+
+            <MdNotInterested className="text-3xl" />
+
+            <p className="text-sm">Không còn chỗ</p>
           </div>
-
-          <p className="font-bold">Economy</p>
-
-          <div className="my-2 flex flex-col items-center">
-            <p>từ</p>
-            <p className="text-xl font-bold">
-              {flightCardData.flightPricing[0].ticketPrice.toLocaleString()}
-            </p>
-            <p>VND</p>
-          </div>
-
-          <MdExpandMore
-            className={`${showEconomyClass ? "rotate-180 text-3xl" : "rotate-0"} transform text-white duration-500`}
-          />
         </div>
-      </div>
+      ) : (
+        <div
+          className="relative flex-1 cursor-pointer bg-green-700"
+          onClick={() => {
+            if (showEconomyClass === false) {
+              handleShowEconomyClass();
+            } else {
+              handleCloseDetailClass();
+            }
+          }}
+        >
+          <div className="flex flex-col items-center py-2 text-white">
+            <div className="absolute -top-[24px] rounded-t-md bg-green-900 px-2 py-1">
+              <p className="text-xs">{`Còn ${availableEconomySeats} chỗ`}</p>
+            </div>
 
-      <div
-        className={`${showBusinessClass || showEconomyClass ? "rounded-tr-lg" : "rounded-r-lg"} relative flex-1 cursor-pointer bg-blue-800 transition-all duration-500`}
-        onClick={() => {
-          if (showBusinessClass === false) {
-            handleShowBusinessClass();
-          } else {
-            handleCloseDetailClass();
-          }
-        }}
-      >
-        <div className="flex flex-col items-center py-2 text-white">
-          <div className="absolute -top-[24px] rounded-t-md bg-blue-900 px-2 py-1">
-            <p className="text-xs">{`Còn ${availableBusinessSeats} chỗ`}</p>
+            <p className="font-bold">Economy</p>
+
+            <div className="my-2 flex flex-col items-center">
+              <p>từ</p>
+              <p className="text-xl font-bold">
+                {flightCardData.flightPricing[0].ticketPrice.toLocaleString()}
+              </p>
+              <p>VND</p>
+            </div>
+
+            <MdExpandMore
+              className={`${showEconomyClass ? "rotate-180 text-3xl" : "rotate-0"} transform text-white duration-500`}
+            />
           </div>
-          <p className="font-bold">Business</p>
-
-          <div className="my-2 flex flex-col items-center">
-            <p>từ</p>
-            {/* props */}
-            <p className="text-xl font-bold">
-              {flightCardData.flightPricing[1].ticketPrice.toLocaleString()}
-            </p>
-            <p>VND</p>
-          </div>
-
-          <MdExpandMore
-            className={`${showBusinessClass ? "rotate-180 text-3xl" : "rotate-0"} transform text-white duration-500`}
-          />
         </div>
-      </div>
+      )}
+
+      {availableBusinessSeats == 0 ? (
+        <div className="relative flex flex-1 cursor-not-allowed items-center justify-center bg-gray-300">
+          <div className="flex flex-col items-center py-8 text-black opacity-70">
+            <p className="font-bold text-blue-800">Business</p>
+
+            <MdNotInterested className="text-3xl" />
+
+            <p className="text-sm">Không còn chỗ</p>
+          </div>
+        </div>
+      ) : (
+        <div
+          className="relative flex-1 cursor-pointer bg-blue-800"
+          onClick={() => {
+            if (showBusinessClass === false) {
+              handleShowBusinessClass();
+            } else {
+              handleCloseDetailClass();
+            }
+          }}
+        >
+          <div className="flex flex-col items-center py-2 text-white">
+            <div className="absolute -top-[24px] rounded-t-md bg-blue-900 px-2 py-1">
+              <p className="text-xs">{`Còn ${availableBusinessSeats} chỗ`}</p>
+            </div>
+            <p className="font-bold">Business</p>
+
+            <div className="my-2 flex flex-col items-center">
+              <p>từ</p>
+              <p className="text-xl font-bold">
+                {flightCardData.flightPricing[1].ticketPrice.toLocaleString()}
+              </p>
+              <p>VND</p>
+            </div>
+
+            <MdExpandMore
+              className={`${showBusinessClass ? "rotate-180 text-3xl" : "rotate-0"} transform text-white duration-500`}
+            />
+          </div>
+        </div>
+      )}
     </>
   );
 };
