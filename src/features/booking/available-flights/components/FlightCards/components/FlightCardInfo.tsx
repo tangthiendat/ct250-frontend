2f7c26ee4056c6cae777c912d5b1765cfdcb5eb0
QuocKhang <phamquockhang1108@gmail.com>
2024-10-17 19:01:39 +0700

    refactor: update FlightCardInfo
    - add FlightIndicator & FlightDetail

--- src/features/booking/available-flights/components/FlightCards/components/FlightCardInfo.tsx
+++ src/features/booking/available-flights/components/FlightCards/components/FlightCardInfo.tsx
@@ -1,112 +1,37 @@
 import { Divider } from "antd";
-import dayjs from "dayjs";
-import { FaRegClock } from "react-icons/fa";
-import { IoIosInformationCircleOutline } from "react-icons/io";
-import { PiAirplaneInFlightFill } from "react-icons/pi";
-import { IFlightSchedule } from "../../../../../../interfaces";
-import DetailFlight from "./DetailFlight";
 import { useState } from "react";
+import { IFlightSchedule } from "../../../../../../interfaces";
+import FlightDetail from "./FlightDetail";
+import FlightDetailOverview from "./FlightDetailOverview";
+import FlightIndicator from "./FlightIndicator";
 
 interface FlightCardInfoProps {
   flightCardData: IFlightSchedule;
 }
 
 const FlightCardInfo: React.FC<FlightCardInfoProps> = ({ flightCardData }) => {
-  const durationInMinutes = dayjs(flightCardData.arrivalDateTime).diff(
-    dayjs(flightCardData.departureDateTime),
-    "minute",
-  );
-  const hours = Math.floor(durationInMinutes / 60);
-  const minutes = durationInMinutes % 60;
-  const formattedDuration =
-    minutes === 0 ? `${hours} giờ` : `${hours} giờ ${minutes} phút`;
   const [showDetail, setShowDetail] = useState<boolean>(false);
 
   return (
     <>
       <div className="flex flex-1 flex-col items-center justify-center">
-        <div className="flex flex-row items-center">
-          <div>
-            <p className="text-heading-3 text-blue-800">
-              {new Date(flightCardData.departureDateTime).toLocaleTimeString(
-                "vi-VN",
-                {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                },
-              )}
-            </p>
-            <p className="text-heading-3 text-blue-800">
-              {flightCardData.route.departureAirport.cityName}
-            </p>
-          </div>
-
-          <div className="mx-2 flex h-3 items-center">
-            <div>
-              <Divider
-                type="horizontal"
-                className="w-16 border-blue-800 md:w-20"
-                dashed
-                variant="dashed"
-              />
-            </div>
-            <PiAirplaneInFlightFill className="text-blue-800" />
-          </div>
-
-          <div>
-            <p className="text-heading-3 text-blue-800">
-              {new Date(flightCardData.arrivalDateTime).toLocaleTimeString(
-                "vi-VN",
-                {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                },
-              )}
-            </p>
-
-            <p className="text-heading-3 text-blue-800">
-              {flightCardData.route.arrivalAirport.cityName}
-            </p>
-          </div>
+        <div className="flex w-[90%] flex-row items-center justify-between">
+          <FlightIndicator flightCardData={flightCardData} />
         </div>
       </div>
 
-      <Divider type="vertical" className="h-12 bg-black" />
+      <Divider type="vertical" className="h-[60%] bg-slate-400" />
 
-      <div className="ml-4 flex flex-1 flex-col items-start text-sm">
-        <p className="flex items-center justify-center">
-          <div>
-            <FaRegClock className="mr-2 text-xs text-blue-800" />
-          </div>
-          <p className="title-4">
-            Thời gian bay:{" "}
-            <span className="font-semibold">{formattedDuration}</span>
-          </p>
-        </p>
-
-        <p className="flex items-center justify-center">
-          <div>
-            <PiAirplaneInFlightFill className="mr-2 text-xs text-blue-800" />
-          </div>
-          <p className="title-4">
-            <span className="font-semibold">
-              {flightCardData.airplane.model.modelName}
-            </span>{" "}
-            được vận hành bởi DaViKa Airways.
-          </p>
-        </p>
-
-        <p className="flex cursor-pointer items-center justify-center">
-          <div>
-            <IoIosInformationCircleOutline className="mr-2 text-xs text-blue-800" />
-          </div>
-          <p className="title-4" onClick={() => setShowDetail(true)}>
-            Xem chi tiết chuyến bay
-          </p>
-        </p>
+      <div className="flex flex-1 items-center justify-center text-sm">
+        <div className="flex w-[90%] flex-col items-start justify-between">
+          <FlightDetail
+            flightCardData={flightCardData}
+            setShowDetail={setShowDetail}
+          />
+        </div>
       </div>
 
-      <DetailFlight
+      <FlightDetailOverview
         flightData={flightCardData}
         open={showDetail}
         setOpen={setShowDetail}
