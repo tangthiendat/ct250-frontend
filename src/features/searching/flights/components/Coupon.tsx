3b3d7d695bba61f6cf9750d2c15f7aff12e78319
QuocKhang <phamquockhang1108@gmail.com>
2024-11-29 23:54:20 +0700

    feat(coupon): add tooltip for coupon code input

--- src/features/searching/flights/components/Coupon.tsx
+++ src/features/searching/flights/components/Coupon.tsx
@@ -1,4 +1,5 @@
-import { Form, Input } from "antd";
+import { Form, Input, Tooltip } from "antd";
+import { IoMdHelpCircle } from "react-icons/io";
 import { RiCoupon3Line } from "react-icons/ri";
 
 interface CouponProps {
@@ -11,9 +12,17 @@ const Coupon: React.FC<CouponProps> = ({ couponCode, setCouponCode }) => {
     <Form.Item name="couponCode" initialValue={couponCode}>
       <Input
         className="w-full"
+        allowClear
         size="large"
         placeholder="Nhập mã giảm giá"
         prefix={<RiCoupon3Line />}
+        suffix={
+          <div className="text-gray-400">
+            <Tooltip title="Mã giảm giá sẽ được áp dụng vào giá vé cơ bản!!">
+              <IoMdHelpCircle className="text-xl" />
+            </Tooltip>
+          </div>
+        }
         onChange={(e) => {
           setCouponCode(e.target.value);
         }}
